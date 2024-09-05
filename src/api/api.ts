ec8f3d528962e1e013c2a58561a42572138f88a2
sdoxiaobaomei <18829355062@163.com>
2024-09-06 02:47:33 +0800

    add cors for json-server

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,6 +1,6 @@
 import request from '../utils/request';
 
-const jsonDbUrl = 'http://localhost:3000';
+const jsonDbUrl = 'http://116.62.155.169:3000';
 
 export const getSeatBook = () => {
     return request({
